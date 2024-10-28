2cc93c4f48e7299f77c64945c7894941810fb8b9
DamianBermudez94 <bermudezdamian7@gamil.com>
2024-10-27 23:26:16 -0300

    se vio flexbox y se estuvo repasasando lo visto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # curso-tailwind-css
+
+1-Input para poder escuchar los cambios de tailwind ( npx tailwindcss -i ./src/input.css -o ./src/output.css --watch )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
       colors:{
         "azul-claro":"#243cff",
         "azul-oscuro":"#0d1b3e"
-      }
+      },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
